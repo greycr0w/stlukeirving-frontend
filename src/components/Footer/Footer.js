39e70cc9c3821f604bdb695f62bb9a763ff9421c
greycr0w <alexbant@teiser.gr>
2019-04-08 04:23:49 +0300

    stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -53,9 +53,13 @@ class Footer extends Component {
                     <div className="col-md-4 footer_widget">
                         <div className="widget_inner">
                             <h5>Contact Us</h5>
-                            <p>Address: 202 S. MacArthur Blvd Irving, Texas 75060</p>
+                            <p className="contact-us-church">Church</p>
+                            <p>202 S. MacArthur Blvd Irving, Texas 75060</p>
+                            <p className="contact-us-church">Rectory</p>
+                            <p>1015 Schulze Drive Irving, Texas 75060</p>
                             <p>Email:  <a href="mailto:contact@stlukeirving.org">conatct@stlukeirving.org</a></p>
                             <p>Phone:  972-259-3222	</p>
+                            <p>Fax: 972-259-3339</p>
                         </div>
                     </div>
                     <div className="col-md-4 footer_widget">
@@ -79,26 +83,7 @@ class Footer extends Component {
                 </div>
             </div>
 
-            <div className="footer_bottom">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <p>&copy; 2019 St. Luke Irving All Rights Reserved</p>
-                        </div>
-                        <div className="col-md-4">
-                          
-                        </div>
-                        <div className="col-md-4">
-                            <div className="footer_links">
-                                <a href="home-4.html#">Home</a>
-                                <a href="home-4.html#">About Us</a>
-                                <a href="home-4.html#">Sermon</a>
-                                <a href="home-4.html#">Events</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </footer>
         )
     }
